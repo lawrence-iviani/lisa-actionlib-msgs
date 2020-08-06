6222cbdfea5264f701f86d730f808ac2cace8b01
Lawrence Iviani <lawrence.iviani@gmail.com>
2020-08-06 22:02:16 +0100

    clean up tutorial

--- scripts/action_client.py
+++ scripts/action_client.py
@@ -4,15 +4,15 @@ import roslib
 # roslib.load_manifest('my_pkg_name')
 import rospy
 import actionlib
+from lisa_actionlib_msgs.msg  import LisaUtterAction, LisaUtterGoal
 
-from dishes_actionlib_example.msg import DoDishesAction, DoDishesGoal
 
 if __name__ == '__main__':
-    rospy.init_node('do_dishes_client')
-    client = actionlib.SimpleActionClient('do_dishes', DoDishesAction)
+    rospy.init_node('lisa_utter_client')
+    client = actionlib.SimpleActionClient('/lisa/say', LisaUtterAction)
     client.wait_for_server()
 
-    goal = DoDishesGoal()
+    goal = LisaUtterGoal(sentence="something to say")
     # Fill in the goal here
     client.send_goal(goal)
     client.wait_for_result(rospy.Duration.from_sec(5.0))
